5f95e958f0cf64e97a2e10ac5ec8ec62c7b293be
Thadeu Guimarães <52017205+Thadeu84@users.noreply.github.com>
2023-03-08 19:18:21 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const metricsMiddleware = promBundle({
         includePath: true, 
         customLabels: 
             { 
-                project_version: '3.0' 
+                project_version: '2.0' 
             } 
     });
 
